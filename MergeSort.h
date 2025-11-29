4e705128a5ba2271b0f24c45ffd3f128e4b1e2ab
KrzysztofDutka04 <97160815+KrzysztofDutka04@users.noreply.github.com>
2025-11-29 17:27:04 +0100

    Remove unused merge functions from MergeSort class

--- MergeSort.h
+++ MergeSort.h
@@ -7,9 +7,7 @@ template<typename T>
 class MergeSort {
 private:
     std::vector<T> array;
-    void merge(int left, int mid, int right);
-    void mergeSort(int left, int right);
-
+    
 public:
     MergeSort(const std::vector<T>& inputArray);
     void sort();
