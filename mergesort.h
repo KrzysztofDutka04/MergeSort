f9233f7d695d1bb1ff0cc81425416c5e4dcdb8e6
KrzysztofDutka04 <97160815+KrzysztofDutka04@users.noreply.github.com>
2025-11-29 14:02:42 +0100

    Refactor MergeSort class by removing includes and methods
    Removed unnecessary includes and a method from the MergeSort class.

--- mergesort.h
+++ mergesort.h
@@ -2,22 +2,18 @@
 #define MERGESORT_H
 
 #include <vector>
-#include <iostream>
 
-template <typename T>
+template<typename T>
 class MergeSort {
 private:
     std::vector<T> array;
-
     void merge(int left, int mid, int right);
     void mergeSort(int left, int right);
 
 public:
     MergeSort(const std::vector<T>& inputArray);
     void sort();
-    std::vector<T> getSortedArray() const;
     void display() const;
-    void setArray(const std::vector<T>& newArray);
 };
 
 #endif
