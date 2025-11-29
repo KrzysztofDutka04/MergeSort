229cea754f48d2f363b439b5cf2223cdee2aa74b
KrzysztofDutka04 <97160815+KrzysztofDutka04@users.noreply.github.com>
2025-11-29 14:01:52 +0100

    Refactor mergesort to use template class

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,57 +1,76 @@
-#include <iostream>
-#include <vector>
 #include "mergesort.h"
+#include <iostream>
 
 using namespace std;
 
-int main() {
-    // Test z liczbami ca³kowitymi (int)
-    vector<int> intArray = {64, 34, 25, 12, 22, 11, 90, 5, 77, 30};
-    
-    cout << "Tablica int przed sortowaniem:" << endl;
-    for (const auto& num : intArray) {
-        cout << num << " ";
+template<typename T>
+MergeSort<T>::MergeSort(const vector<T>& inputArray) : array(inputArray) {}
+
+template<typename T>
+void MergeSort<T>::sort() {
+    if (array.size() > 1) {
+        mergeSort(0, array.size() - 1);
     }
-    cout << endl;
-
-    MergeSort<int> intSorter(intArray);
-    intSorter.sort();
-    
-    cout << "Tablica int po sortowaniu:" << endl;
-    intSorter.display();
-    cout << endl;
-
-    // Test z liczbami zmiennoprzecinkowymi (double)
-    vector<double> doubleArray = {64.5, 34.2, 25.1, 12.7, 22.8, 11.9, 90.3, 5.6, 77.4, 30.0};
-    
-    cout << "Tablica double przed sortowaniem:" << endl;
-    for (const auto& num : doubleArray) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    MergeSort<double> doubleSorter(doubleArray);
-    doubleSorter.sort();
-    
-    cout << "Tablica double po sortowaniu:" << endl;
-    doubleSorter.display();
-    cout << endl;
-
-    // Dodatkowe testy z innymi typami
-    vector<long> longArray = {1000L, 500L, 2000L, 100L, 3000L};
-    MergeSort<long> longSorter(longArray);
-    longSorter.sort();
-    
-    cout << "Tablica long po sortowaniu:" << endl;
-    longSorter.display();
-    cout << endl;
-
-    vector<float> floatArray = {3.14f, 2.71f, 1.41f, 1.73f, 0.57f};
-    MergeSort<float> floatSorter(floatArray);
-    floatSorter.sort();
-    
-    cout << "Tablica float po sortowaniu:" << endl;
-    floatSorter.display();
-
-    return 0;
 }
+
+template<typename T>
+void MergeSort<T>::mergeSort(int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(left, mid);
+        mergeSort(mid + 1, right);
+        merge(left, mid, right);
+    }
+}
+
+template<typename T>
+void MergeSort<T>::merge(int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<T> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = array[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = array[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            array[k] = L[i];
+            i++;
+        } else {
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+template<typename T>
+void MergeSort<T>::display() const {
+    for (size_t i = 0; i < array.size(); i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+// Jawne instancje szablonów
+template class MergeSort<int>;
+template class MergeSort<double>;
+template class MergeSort<long>;
+template class MergeSort<float>;
